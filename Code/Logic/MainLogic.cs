e64cabd111fa985be9932a0df9f3ff5e8b603436
Inevitabilis <ddd1020018@gmail.com>
2024-02-05 03:16:31 +0300

    split repo into code and mod
    for better working and testing pipeline

---
+++ Code/Logic/MainLogic.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using Menu;
+using static PVStuffMod.StaticStuff;
+
+namespace PVStuffMod;
+
+internal static class MainLogic
+{
+
+    static internal void Startup()
+    {
+        PlayerLogic.ApplyHooks();
+        On.Menu.MenuScene.BuildScene += MenuScene_BuildScene;
+
+    }
+    static internal void RegisterROMObjects()
+    {
+
+    }
+
+    static internal void HackySolutions()
+    {
+        On.Menu.SlugcatSelectMenu.Update += static (On.Menu.SlugcatSelectMenu.orig_Update orig, Menu.SlugcatSelectMenu self) =>
+        {
+            orig(self);
+            if (Input.GetKeyUp("g"))
+            {
+                SlugcatStats.Name currentlySelectedSinglePlayerSlugcat = self.manager.rainWorld.progression.miscProgressionData.currentlySelectedSinglePlayerSlugcat;
+                SaveState saveState = self.manager.rainWorld.progression.GetOrInitiateSaveState(currentlySelectedSinglePlayerSlugcat, null, self.manager.menuSetup, false);
+
+            }
+        };
+    }
+    private static void MenuScene_BuildScene(On.Menu.MenuScene.orig_BuildScene orig, MenuScene self)
+    {
+        if(self.menu is SlugcatSelectMenu && (devBuild || EscapismEnding.Contains(self.sceneID.GetCharacterFromSelectScene())))
+            self.sceneID = self.sceneID.GetCharacterFromSelectScene().GetSelectScreenSceneID();
+        orig(self);
+    }
+}
---
+++ Code/StaticStuff.cs
@@ -0,0 +1,75 @@
+﻿using Menu;
+using System.Collections.Generic;
+using MoreSlugcats;
+using static SlugcatStats.Name;
+using static MoreSlugcats.MoreSlugcatsEnums.SlugcatStatsName;
+using static MoreSlugcats.MoreSlugcatsEnums.MenuSceneID;
+using static Menu.MenuScene.SceneID;
+
+namespace PVStuffMod;
+
+public static class StaticStuff
+{
+    public const bool devBuild = true;
+    public const string head = "Preservatory_";
+    public const string tail = "EscEnd";
+    internal const string stasisRoomName = "SWIM";
+    internal static HashSet<SlugcatStats.Name> EscapismEnding = new();
+    public class EscapismEndingSlugcatScreen : ExtEnum<EscapismEndingSlugcatScreen>
+    {
+        public EscapismEndingSlugcatScreen(string value, bool register = false) : base(value, register) { }
+        public static MenuScene.SceneID Artificer = new(head + nameof(Artificer) + tail, false);
+        public static MenuScene.SceneID Gourmand = new(head +nameof(Gourmand) + tail, false);
+        public static MenuScene.SceneID Spearmaster = new(head + nameof(Spearmaster) + tail, false);
+        public static MenuScene.SceneID Survivor = new(head + nameof(Survivor) + tail, false);
+        public static MenuScene.SceneID Monk = new(head + nameof(Monk) + tail, false);
+        public static MenuScene.SceneID Rivulet = new(head + nameof(Rivulet) + tail, false);
+        public static MenuScene.SceneID Hunter = new(head + nameof(Hunter) + tail, false);
+
+    }
+    internal static Dictionary<MenuScene.SceneID, SlugcatStats.Name> sceneNameMap = new()
+    {
+        { Ghost_Yellow, Yellow },
+        { Slugcat_Yellow, Yellow },
+        { AltEnd_Monk, Yellow },
+        { Ghost_Red, Red },
+        { Slugcat_Red, Red },
+        { Slugcat_Gourmand, Gourmand },
+        { End_Gourmand, Gourmand },
+        { AltEnd_Gourmand, Gourmand },
+        { AltEnd_Gourmand_Full, Gourmand },
+        { End_Spear, MoreSlugcatsEnums.SlugcatStatsName.Spear },
+        { AltEnd_Spearmaster, MoreSlugcatsEnums.SlugcatStatsName.Spear },
+        { Slugcat_Spear, MoreSlugcatsEnums.SlugcatStatsName.Spear },
+        { Slugcat_Artificer, Artificer },
+        { Slugcat_Artificer_Robo, Artificer },
+        { Slugcat_Artificer_Robo2, Artificer },
+        { AltEnd_Artificer_Portrait, Artificer },
+        { End_Artificer, Artificer },
+        { End_Rivulet, Rivulet },
+        { AltEnd_Rivulet, Rivulet },
+        { AltEnd_Rivulet_Robe, Rivulet },
+        { Slugcat_Saint, Saint },
+        { SaintMaxKarma, Saint },
+        { End_Saint, Saint },
+        { Slugcat_White, SlugcatStats.Name.White }
+    };
+    internal static Dictionary<SlugcatStats.Name, MenuScene.SceneID> nameSceneMap = new()
+    {
+        { Yellow, EscapismEndingSlugcatScreen.Monk },
+        { Red, EscapismEndingSlugcatScreen.Hunter },
+        { Gourmand, EscapismEndingSlugcatScreen.Gourmand },
+        { MoreSlugcatsEnums.SlugcatStatsName.Spear, EscapismEndingSlugcatScreen.Spearmaster },
+        { Artificer, EscapismEndingSlugcatScreen.Artificer },
+        { Rivulet, EscapismEndingSlugcatScreen.Rivulet }
+    };
+
+    static internal SlugcatStats.Name GetCharacterFromSelectScene(this MenuScene.SceneID sceneID)
+    {
+        return sceneNameMap.TryGetValue(sceneID, out var name) ? name : White;
+    }
+    static internal MenuScene.SceneID GetSelectScreenSceneID(this SlugcatStats.Name character)
+    {
+        return nameSceneMap.TryGetValue(character, out var sceneID) ? sceneID : EscapismEndingSlugcatScreen.Survivor;
+    }
+}
